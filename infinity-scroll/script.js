11bcec74354ce5234b0cad3e2f91ac2477c7f0bd
Dharan0512 <dharanibala0512@gmail.com>
2022-12-23 15:38:09 +0530

    joke-teller done

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -9,7 +9,7 @@
  let photosArray = [];
  
  // Unsplash API
- const count = 10;
+ const count = 5;
  const apiKey = 'KZ9FTGvbBc80jnKvGUQtSrGMi50zxTcFihKnyCyXv3s';
  const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
  
